7724de6c83081a0807b2267c66c466a16057a6ec
df7c5117 <df7c5117@gmail.com>
2021-11-23 00:12:45 +0800

    update1123

--- js/core.js
+++ js/core.js
@@ -1,20 +1,70 @@
-Zepto(function($) {
-	$(window).resize(function(){
+function stickSidebar(){
+	Zepto($ => {
 		if(document.body.clientWidth >= 980){
 			$('#foot').stickUp();
-		}else{
-			$('#foot').removeClass('stuckMenu');
-			$('#foot').removeClass('isStuck');
-			$('#foot').css({"position":'','top':''});
 		}
+		$(window).resize(function(){
+			if(document.body.clientWidth >= 980){
+				$('#foot').stickUp();				
+				$('#foot').addClass('stuckMenu');				
+				$('#foot').addClass('isStuck');
+				if(vartop < varscroll){
+					$('#foot').css({"position":'fixed','top':'0px;'});
+				}
+			}else{
+				$('#foot').removeClass('stuckMenu');
+				$('#foot').removeClass('isStuck');
+				$('#foot').css({"position":'','top':''});
+			}
+		});
 	});
-});
+}
 
-function GetLinkArgs()
-{
+function getLinkArgs(){
 	var reg = new RegExp("(^|&)([^&]*)(&|$)");
 	var r = window.location.search.substr(1).match(reg);//search,查询？后面的参数，并匹配正则
 	if(r != null)return  unescape(r[2]); return null;
+};
+
+
+let currentArti = getLinkArgs();
+
+function getIndexMeta(){
+	axios
+		.get('./docs/list.json')
+		.then(response => {
+			app.arti = response.data.arti;
+			app.jumboItems = response.data.jumboItems;
+			app.topics = response.data.topics;
+			app.specials = response.data.specials;
+			app.categories = response.data.categories
+		});
 }
-var currentArti = GetLinkArgs();
+
+function getArtiMeta(type, linkArg){
+	axios
+		.get('../docs/' + type + '/' + linkArg + '/meta.json')
+		.then(response => {
+			app.meta = response.data.meta;
+			app.isExpand = response.data.isExpand;
+			app.expand = response.data.expand;
+			app.author = response.data.author;
+			document.title = response.data.pagetitle + " / 中原铁道(数字)博物馆";
+		});
+}
+
+function renderMarkdown(type, linkArg) {
+	axios
+		.get('../docs/' + type + '/' + linkArg + '/' + linkArg + '.md')
+		.then(response => {
+			var Convertor = new showdown.Converter();
+			$('#markdown').html(Convertor.makeHtml(response.data));
+			$('#author').removeClass('d-none');
+		})
+		.catch(error => {
+			$('#NotFound').removeClass('d-none');
+			$('#NotFound').addClass('d-flex');
+		});
+}
+
 
