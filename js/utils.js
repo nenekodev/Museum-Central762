d79f3ec0c5e707cb8d2056f253e8f157a519cfac
df7c5117 <df7c5117@gmail.com>
2021-12-05 12:52:55 +0800

    1205 vue3

---
+++ js/utils.js
@@ -0,0 +1,44 @@
+function stickSidebar() {
+  Zepto(($) => {
+    if (document.body.clientWidth >= 980) {
+      $('#foot').stickUp()
+    }
+    $(window).resize(function () {
+      if (document.body.clientWidth >= 980) {
+        $('#foot').stickUp()
+        $('#foot').addClass('stuckMenu')
+        $('#foot').addClass('isStuck')
+        if (vartop < varscroll) {
+          $('#foot').css({ position: 'fixed', top: '0px;' })
+        }
+      } else {
+        $('#foot').removeClass('stuckMenu')
+        $('#foot').removeClass('isStuck')
+        $('#foot').css({ position: '', top: '' })
+      }
+    })
+  })
+}
+
+function getLinkArgs() {
+  var reg = new RegExp('(^|&)([^&]*)(&|$)')
+  var r = window.location.search.substr(1).match(reg) //search,查询？后面的参数，并匹配正则
+  if (r != null) return unescape(r[2])
+  return null
+}
+
+let currentArti = getLinkArgs()
+
+function renderMarkdown(type, linkArg) {
+  axios
+    .get('../docs/' + type + '/' + linkArg + '/' + linkArg + '.md')
+    .then((response) => {
+      var Convertor = new showdown.Converter()
+      $('#markdown').html(Convertor.makeHtml(response.data))
+      $('#author').removeClass('d-none')
+    })
+    .catch((error) => {
+      $('#NotFound').removeClass('d-none')
+      $('#NotFound').addClass('d-flex')
+    })
+}
